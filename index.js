5f86917272dc9b5a4a4b22bd69d074f8d5b4ae30
Daniel Schneider <daschne8@gmail.com>
2019-06-15 17:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function justInvoke(cb){
 function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg)
 }
+
+function setThisWithApply(fn, thisValue, arg){
+  return fn.apply(thisValue, arg)
+}
